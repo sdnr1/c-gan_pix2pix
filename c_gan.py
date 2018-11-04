58da0e941f118d68d3dc19f7a15b139dd5f67500
sdnr1 <sidnayyar@outlook.com>
2018-11-04 16:07:09 +0530

    Add files via upload

---
+++ c_gan.py
@@ -0,0 +1,398 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import os
+import time
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+
+BATCH_SIZE = 1
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+LAMBDA = 100
+OUTPUT_CHANNELS = 3
+checkpoint_dir = './training_checkpoints'
+output_dir = './generated_images'
+
+
+class Downsample(tf.keras.Model):
+    
+    def __init__(self, filters, size, apply_batchnorm=True):
+        super(Downsample, self).__init__()
+        self.apply_batchnorm = apply_batchnorm
+        initializer = tf.random_normal_initializer(0., 0.02)
+
+        self.conv1 = tf.keras.layers.Conv2D(filters,
+                                            (size, size),
+                                            strides=2,
+                                            padding='same',
+                                            kernel_initializer=initializer,
+                                            use_bias=False)
+        if self.apply_batchnorm:
+            self.batchnorm = tf.keras.layers.BatchNormalization()
+  
+    def call(self, x, training):
+        x = self.conv1(x)
+        if self.apply_batchnorm:
+            x = self.batchnorm(x, training=training)
+        x = tf.nn.leaky_relu(x)
+        return x
+
+
+class Upsample(tf.keras.Model):
+    
+    def __init__(self, filters, size, apply_dropout=False):
+        super(Upsample, self).__init__()
+        self.apply_dropout = apply_dropout
+        initializer = tf.random_normal_initializer(0., 0.02)
+
+        self.up_conv = tf.keras.layers.Conv2DTranspose(filters,
+                                                       (size, size),
+                                                       strides=2,
+                                                       padding='same',
+                                                       kernel_initializer=initializer,
+                                                       use_bias=False)
+        self.batchnorm = tf.keras.layers.BatchNormalization()
+        if self.apply_dropout:
+            self.dropout = tf.keras.layers.Dropout(0.5)
+
+    def call(self, x1, x2, training):
+        x = self.up_conv(x1)
+        x = self.batchnorm(x, training=training)
+        if self.apply_dropout:
+            x = self.dropout(x, training=training)
+        x = tf.nn.relu(x)
+        if x2 is not None:
+            x = tf.concat([x, x2], axis=-1)
+        return x
+
+
+class ResidualBlock(tf.keras.Model):
+    
+    def __init__(self, filters, size, norm_axis=None):
+        super(ResidualBlock, self).__init__()
+
+        self.conv1 = tf.keras.layers.Conv2D(filters,
+                                            (size, size),
+                                            padding='same',
+                                            kernel_initializer='he_normal')
+
+        self.batchnorm1 = tf.keras.layers.BatchNormalization(axis=norm_axis)
+        
+        self.conv2 = tf.keras.layers.Conv2D(filters,
+                                            (size, size),
+                                            padding='same',
+                                            kernel_initializer='he_normal')
+        
+        self.batchnorm2 = tf.keras.layers.BatchNormalization(axis=norm_axis)
+  
+    def call(self, x, training):
+        y = self.conv1(x)
+        y = self.batchnorm1(y, training=training)
+        y = tf.nn.relu(y)
+        y = self.conv2(y)
+        y = self.batchnorm2(y, training=training)
+        y = tf.add(x, y)
+        y = tf.nn.relu(y)
+        return y
+
+
+class UNetGenerator(tf.keras.Model):
+    
+    def __init__(self, noise=False):
+        super(UNetGenerator, self).__init__()
+        self.noise_inputs = noise
+        initializer = tf.random_normal_initializer(0., 0.02)
+    
+        self.down1 = Downsample(64, 4, apply_batchnorm=False)
+        self.down2 = Downsample(128, 4)
+        self.down3 = Downsample(256, 4)
+        self.down4 = Downsample(512, 4)
+        self.down5 = Downsample(512, 4)
+        self.down6 = Downsample(512, 4)
+        self.down7 = Downsample(512, 4)
+        self.down8 = Downsample(512, 4)
+
+        self.up1 = Upsample(512, 4, apply_dropout=True)
+        self.up2 = Upsample(512, 4, apply_dropout=True)
+        self.up3 = Upsample(512, 4, apply_dropout=True)
+        self.up4 = Upsample(512, 4)
+        self.up5 = Upsample(256, 4)
+        self.up6 = Upsample(128, 4)
+        self.up7 = Upsample(64, 4)
+
+        self.last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS,
+                                                    (4, 4),
+                                                    strides=2,
+                                                    padding='same',
+                                                    kernel_initializer=initializer)
+  
+    @tf.contrib.eager.defun
+    def call(self, x, training):
+        # x shape == (bs, 256, 256, 3)
+        if self.noise_inputs:
+            z = tf.random_normal(shape=[BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, 1])
+            x = tf.concat([x, z], axis=-1) # (bs, 256, 256, 4)
+        
+        x1 = self.down1(x, training=training) # (bs, 128, 128, 64)
+        x2 = self.down2(x1, training=training) # (bs, 64, 64, 128)
+        x3 = self.down3(x2, training=training) # (bs, 32, 32, 256)
+        x4 = self.down4(x3, training=training) # (bs, 16, 16, 512)
+        x5 = self.down5(x4, training=training) # (bs, 8, 8, 512)
+        x6 = self.down6(x5, training=training) # (bs, 4, 4, 512)
+        x7 = self.down7(x6, training=training) # (bs, 2, 2, 512)
+        x8 = self.down8(x7, training=training) # (bs, 1, 1, 512)
+
+        x9 = self.up1(x8, x7, training=training) # (bs, 2, 2, 1024)
+        x10 = self.up2(x9, x6, training=training) # (bs, 4, 4, 1024)
+        x11 = self.up3(x10, x5, training=training) # (bs, 8, 8, 1024)
+        x12 = self.up4(x11, x4, training=training) # (bs, 16, 16, 1024)
+        x13 = self.up5(x12, x3, training=training) # (bs, 32, 32, 512)
+        x14 = self.up6(x13, x2, training=training) # (bs, 64, 64, 256)
+        x15 = self.up7(x14, x1, training=training) # (bs, 128, 128, 128)
+
+        x16 = self.last(x15) # (bs, 256, 256, 3)
+        x16 = tf.nn.tanh(x16)
+
+        return x16
+
+
+class ResNet9Generator(tf.keras.Model):
+    
+    def __init__(self, noise=False):
+        super(ResNet9Generator, self).__init__()
+        self.noise_inputs = noise
+        initializer = tf.random_normal_initializer(0., 0.02)
+        
+        self.init = tf.keras.layers.Conv2D(64,
+                                           (7, 7),
+                                           padding='same',
+                                           kernel_initializer=initializer)
+        
+        self.down1 = Downsample(128, 4)
+        self.down2 = Downsample(256, 4)
+        
+        self.res1 = ResidualBlock(256, 3, norm_axis=3)
+        self.res2 = ResidualBlock(256, 3, norm_axis=3)
+        self.res3 = ResidualBlock(256, 3, norm_axis=3)
+        self.res4 = ResidualBlock(256, 3, norm_axis=3)
+        self.res5 = ResidualBlock(256, 3, norm_axis=3)
+        self.res6 = ResidualBlock(256, 3, norm_axis=3)
+        self.res7 = ResidualBlock(256, 3, norm_axis=3)
+        self.res8 = ResidualBlock(256, 3, norm_axis=3)
+        self.res9 = ResidualBlock(256, 3, norm_axis=3)
+
+        self.up1 = Upsample(128, 4)
+        self.up2 = Upsample(64, 4)
+
+        self.last = tf.keras.layers.Conv2D(OUTPUT_CHANNELS,
+                                           (7, 7),
+                                           padding='same',
+                                           kernel_initializer=initializer)
+  
+    @tf.contrib.eager.defun
+    def call(self, x, training):
+        # x shape == (bs, 256, 256, 3)
+        if self.noise_inputs:
+            z = tf.random_normal(shape=[BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, 1])
+            x = tf.concat([x, z], axis=-1) # (bs, 256, 256, 4)
+        
+        x1 = self.init(x) # (bs, 256, 256, 64)
+        x1 = tf.nn.relu(x)
+        
+        x2 = self.down1(x1, training=training) # (bs, 128, 128, 128)
+        x3 = self.down2(x2, training=training) # (bs, 64, 64, 256)
+        
+        x4 = self.res1(x3, training=training)
+        x5 = self.res2(x4, training=training)
+        x6 = self.res3(x5, training=training)
+        x7 = self.res4(x6, training=training)
+        x8 = self.res5(x7, training=training)
+        x9 = self.res6(x8, training=training)
+        x10 = self.res7(x9, training=training)
+        x11 = self.res8(x10, training=training)
+        x12 = self.res9(x11, training=training)
+        
+        x13 = self.up1(x12, None, training=training) # (bs, 128, 128, 128)
+        x14 = self.up2(x13, None, training=training) # (bs, 256, 256, 64)
+
+        x15 = self.last(x14) # (bs, 256, 256, 3)
+        x15 = tf.nn.tanh(x15)
+
+        return x15
+
+
+class Discriminator(tf.keras.Model):
+    
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        initializer = tf.random_normal_initializer(0., 0.02)
+    
+        self.down1 = Downsample(64, 4, False)
+        self.down2 = Downsample(128, 4)
+        self.down3 = Downsample(256, 4)
+    
+        # we are zero padding here with 1 because we need our shape to 
+        # go from (batch_size, 32, 32, 256) to (batch_size, 31, 31, 512)
+        self.zero_pad1 = tf.keras.layers.ZeroPadding2D()
+        self.conv = tf.keras.layers.Conv2D(512, 
+                                           (4, 4), 
+                                           strides=1, 
+                                           kernel_initializer=initializer, 
+                                           use_bias=False)
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
+    
+        # shape change from (batch_size, 31, 31, 512) to (batch_size, 30, 30, 1)
+        self.zero_pad2 = tf.keras.layers.ZeroPadding2D()
+        self.last = tf.keras.layers.Conv2D(1, 
+                                           (4, 4), 
+                                           strides=1,
+                                           kernel_initializer=initializer)
+  
+    @tf.contrib.eager.defun
+    def call(self, inp, tar, training):
+        # concatenating the input and the target
+        x = tf.concat([inp, tar], axis=-1) # (bs, 256, 256, channels*2)
+        x = self.down1(x, training=training) # (bs, 128, 128, 64)
+        x = self.down2(x, training=training) # (bs, 64, 64, 128)
+        x = self.down3(x, training=training) # (bs, 32, 32, 256)
+
+        x = self.zero_pad1(x) # (bs, 34, 34, 256)
+        x = self.conv(x)      # (bs, 31, 31, 512)
+        x = self.batchnorm1(x, training=training)
+        x = tf.nn.leaky_relu(x)
+
+        x = self.zero_pad2(x) # (bs, 33, 33, 512)
+        # don't add a sigmoid activation here since
+        # the loss function expects raw logits.
+        x = self.last(x)      # (bs, 30, 30, 1)
+
+        return x
+
+
+class C_GAN():
+    
+    def __init__(self, generator, noise=False):
+        self.name = generator + str(noise) + '_'
+        self.generator = UNetGenerator(noise) if generator == 'unet' else ResNet9Generator(noise)
+        self.discriminator = Discriminator()
+        
+        self.generator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
+        self.discriminator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
+        
+        self.checkpoint_prefix = os.path.join(checkpoint_dir, self.name + "ckpt")
+        self.checkpoint = tf.train.Checkpoint(generator_optimizer=self.generator_optimizer,
+                                              discriminator_optimizer=self.discriminator_optimizer,
+                                              generator=self.generator,
+                                              discriminator=self.discriminator)
+        
+    
+    def generator_loss(self, disc_generated_output, gen_output, target):
+        gan_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels = tf.ones_like(disc_generated_output),
+                                             logits = disc_generated_output) 
+        # mean absolute error
+        l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
+
+        total_gen_loss = gan_loss + (LAMBDA * l1_loss)
+
+        return total_gen_loss
+    
+
+    def discriminator_loss(self, disc_real_output, disc_generated_output):
+        real_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels = tf.ones_like(disc_real_output), 
+                                              logits = disc_real_output)
+        generated_loss = tf.losses.sigmoid_cross_entropy(multi_class_labels = tf.zeros_like(disc_generated_output), 
+                                                   logits = disc_generated_output)
+
+        total_disc_loss = real_loss + generated_loss
+
+        return total_disc_loss
+    
+    
+    def generate_image(self, input_image, target, name):
+        # the training=True is intentional here since
+        # we want the batch statistics while running the model
+        # on the test dataset. If we use training=False, we will get 
+        # the accumulated statistics learned from the training dataset
+        # (which we don't want)
+        gen_output = self.generator(input_image, training=True)
+        plt.figure(figsize=(15,15))
+
+        display_list = [input_image[0], target[0], gen_output[0]]
+        title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+        for i in range(3):
+            plt.subplot(1, 3, i+1)
+            plt.title(title[i])
+            # getting the pixel values between [0, 1] to plot it.
+            plt.imshow(display_list[i] * 0.5 + 0.5)
+            plt.axis('off')
+
+        plt.show()
+        plt.savefig(os.path.join(output_dir, name + '.png'))
+        
+    
+    def restore_from_checkpoint(self):
+        # restoring the latest checkpoint in checkpoint_dir
+        self.checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
+    
+    
+    def train(self, dataset, epochs):
+        training_stats = []
+        
+        for epoch in range(epochs):
+            avg_gen_loss = 0
+            avg_disc_loss = 0
+            it = 0
+            start = time.time()
+            
+            for target, input_image in dataset:
+                it += 1
+                with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                    gen_output = self.generator(input_image, training=True)
+                    
+                    disc_real_output = self.discriminator(input_image, target, training=True)
+                    disc_generated_output = self.discriminator(input_image, gen_output, training=True)
+                    
+                    gen_loss = self.generator_loss(disc_generated_output, gen_output, target)
+                    disc_loss = self.discriminator_loss(disc_real_output, disc_generated_output)
+                    avg_gen_loss += gen_loss.numpy()
+                    avg_disc_loss += disc_loss.numpy()
+                
+                generator_gradients = gen_tape.gradient(gen_loss, 
+                                                        self.generator.variables)
+                discriminator_gradients = disc_tape.gradient(disc_loss, 
+                                                             self.discriminator.variables)
+                
+                self.generator_optimizer.apply_gradients(zip(generator_gradients, 
+                                                             self.generator.variables))
+                self.discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                                                 self.discriminator.variables))
+            
+            time_taken = time.time() - start
+            avg_gen_loss /= it
+            avg_disc_loss /= it
+            training_stats.append((epoch + 1, time_taken, avg_gen_loss, avg_disc_loss))
+            
+            if (epoch + 1) % 20 == 0:
+                self.checkpoint.save(file_prefix = self.checkpoint_prefix)
+                with open(self.name + 'training_stats.pickle', 'wb') as f:
+                    pickle.dump(training_stats, f)
+                
+            
+            print('Time taken for epoch {} is {} sec. Gen_Loss: {} Disc_Loss: {} '.format(epoch + 1, time_taken, avg_gen_loss, avg_disc_loss))
+    
+    
+    def evaluate(self, dataset):
+        avg_mse = 0
+        it = 0
+        
+        for target, input_image in dataset:
+            it += 1
+            gen_output = self.generator(input_image, training=True)
+            avg_mse += tf.reduce_mean(tf.squared_difference(target, gen_output)).numpy()
+        
+        avg_mse /= it
+        return avg_mse
---
+++ initializeDatabase.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+import os
+import numpy as np
+
+BUFFER_SIZE = 400
+BATCH_SIZE = 1
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+def load_image(image_file, is_train):
+    image = tf.read_file(image_file)
+    image = tf.image.decode_jpeg(image)
+    
+    w = tf.shape(image)[1]
+    
+    w = w // 2
+    real_image = image[:, :w, :]
+    input_image = image[:, w:, :]
+
+    input_image = tf.cast(input_image, tf.float32)
+    real_image = tf.cast(real_image, tf.float32)
+
+    if is_train:
+        # random jittering
+    
+        # resizing to 286 x 286 x 3
+        input_image = tf.image.resize_images(input_image, [286, 286], 
+                                             align_corners=True,
+                                             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        real_image = tf.image.resize_images(real_image, [286, 286],
+                                            align_corners=True,
+                                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    
+        # randomly cropping to 256 x 256 x 3
+        stacked_image = tf.stack([input_image, real_image], axis=0)
+        cropped_image = tf.random_crop(stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+        input_image, real_image = cropped_image[0], cropped_image[1]
+
+        if np.random.random() > 0.5:
+            # random mirroring
+            input_image = tf.image.flip_left_right(input_image)
+            real_image = tf.image.flip_left_right(real_image)
+    else:
+        input_image = tf.image.resize_images(input_image, size=[IMG_HEIGHT, IMG_WIDTH],
+                                             align_corners=True,
+                                             method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        real_image = tf.image.resize_images(real_image, size=[IMG_HEIGHT, IMG_WIDTH],
+                                            align_corners=True,
+                                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  
+    # normalizing the images to [-1, 1]
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
+
+    return input_image, real_image
+
+
+def load_database():
+    tf.set_random_seed(1234)
+    np.random.seed(4321)
+
+    path_to_zip = tf.keras.utils.get_file('maps.tar.gz',
+                                        cache_subdir=os.path.abspath('.'),
+                                        origin='https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/maps.tar.gz', 
+                                        extract=True)
+
+    PATH = os.path.join(os.path.dirname(path_to_zip), 'maps/')
+
+    train_dataset = tf.data.Dataset.list_files(PATH+'train/*.jpg')
+    train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+    train_dataset = train_dataset.map(lambda x: load_image(x, True))
+    train_dataset = train_dataset.batch(1)
+
+    test_dataset = tf.data.Dataset.list_files(PATH+'val/*.jpg')
+    test_dataset = test_dataset.map(lambda x: load_image(x, False))
+    test_dataset = test_dataset.batch(1)
+
+    return train_dataset, test_dataset
