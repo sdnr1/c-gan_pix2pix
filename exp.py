c7ae0aadf3f5b3554f6015b4689293184ab2c0c6
sdnr1 <sidnayyar@outlook.com>
2018-11-05 02:12:50 +0530

    Update exp.py

--- exp.py
+++ exp.py
@@ -10,24 +10,8 @@ EPOCHS = 200
 train_dataset, test_dataset = load_database()
 
 
-# U-NET GENERATOR W/O NOISE
-unet_gan = C_GAN(generator='unet')
-unet_gan.train(train_dataset, EPOCHS)
-
-idx = 0
-for tar, img in test_dataset.take(10):
-    idx += 1
-    unet_gan.generate_image(img, tar, name='unet_img' + str(idx))
-
-mse = unet_gan.evaluate(test_dataset)
-with open('mse_unet.pickle', 'wb') as f:
-    pickle.dump(mse, f)
-
-print("Mean Squared Error:", mse)
-
-
 # RESNET-9 GENERATOR W/O NOISE
-resnet_gan = C_GAN(generator='unet')
+resnet_gan = C_GAN(generator='resnet')
 resnet_gan.train(train_dataset, EPOCHS)
 
 idx = 0
