821c1325c3f8e8dad7e2a9ba1075fdaf9e18d874
sdnr1 <sidnayyar@outlook.com>
2018-11-06 17:39:32 +0530

    Add files via upload

--- c_gan.py
+++ c_gan.py
@@ -71,22 +71,22 @@ class Upsample(tf.keras.Model):
 
 class ResidualBlock(tf.keras.Model):
     
-    def __init__(self, filters, size, norm_axis=None):
+    def __init__(self, filters, size):
         super(ResidualBlock, self).__init__()
 
         self.conv1 = tf.keras.layers.Conv2D(filters,
                                             (size, size),
                                             padding='same',
-                                            kernel_initializer='he_normal')
-
-        self.batchnorm1 = tf.keras.layers.BatchNormalization(axis=norm_axis)
+                                            kernel_initializer='he_normal',
+                                            use_bias=False)
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
         
         self.conv2 = tf.keras.layers.Conv2D(filters,
                                             (size, size),
                                             padding='same',
-                                            kernel_initializer='he_normal')
-        
-        self.batchnorm2 = tf.keras.layers.BatchNormalization(axis=norm_axis)
+                                            kernel_initializer='he_normal',
+                                            use_bias=False)
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
   
     def call(self, x, training):
         y = self.conv1(x)
@@ -94,8 +94,7 @@ class ResidualBlock(tf.keras.Model):
         y = tf.nn.relu(y)
         y = self.conv2(y)
         y = self.batchnorm2(y, training=training)
-        y = tf.add(x, y)
-        y = tf.nn.relu(y)
+        y = tf.math.add(x, y)
         return y
 
 
@@ -169,20 +168,22 @@ class ResNet9Generator(tf.keras.Model):
         self.init = tf.keras.layers.Conv2D(64,
                                            (7, 7),
                                            padding='same',
-                                           kernel_initializer=initializer)
+                                           kernel_initializer=initializer,
+                                           use_bias=False)
+        self.batchnorm = tf.keras.layers.BatchNormalization()
         
         self.down1 = Downsample(128, 4)
         self.down2 = Downsample(256, 4)
         
-        self.res1 = ResidualBlock(256, 3, norm_axis=3)
-        self.res2 = ResidualBlock(256, 3, norm_axis=3)
-        self.res3 = ResidualBlock(256, 3, norm_axis=3)
-        self.res4 = ResidualBlock(256, 3, norm_axis=3)
-        self.res5 = ResidualBlock(256, 3, norm_axis=3)
-        self.res6 = ResidualBlock(256, 3, norm_axis=3)
-        self.res7 = ResidualBlock(256, 3, norm_axis=3)
-        self.res8 = ResidualBlock(256, 3, norm_axis=3)
-        self.res9 = ResidualBlock(256, 3, norm_axis=3)
+        self.res1 = ResidualBlock(256, 3)
+        self.res2 = ResidualBlock(256, 3)
+        self.res3 = ResidualBlock(256, 3)
+        self.res4 = ResidualBlock(256, 3)
+        self.res5 = ResidualBlock(256, 3)
+        self.res6 = ResidualBlock(256, 3)
+        self.res7 = ResidualBlock(256, 3)
+        self.res8 = ResidualBlock(256, 3)
+        self.res9 = ResidualBlock(256, 3)
 
         self.up1 = Upsample(128, 4)
         self.up2 = Upsample(64, 4)
@@ -200,28 +201,29 @@ class ResNet9Generator(tf.keras.Model):
             x = tf.concat([x, z], axis=-1) # (bs, 256, 256, 4)
         
         x1 = self.init(x) # (bs, 256, 256, 64)
-        x1 = tf.nn.relu(x)
+        x1 = self.batchnorm(x1, training=training)
+        x1 = tf.nn.relu(x1)
         
         x2 = self.down1(x1, training=training) # (bs, 128, 128, 128)
         x3 = self.down2(x2, training=training) # (bs, 64, 64, 256)
         
         x4 = self.res1(x3, training=training)
-        x5 = self.res2(x4, training=training)
-        x6 = self.res3(x5, training=training)
-        x7 = self.res4(x6, training=training)
-        x8 = self.res5(x7, training=training)
-        x9 = self.res6(x8, training=training)
-        x10 = self.res7(x9, training=training)
-        x11 = self.res8(x10, training=training)
-        x12 = self.res9(x11, training=training)
+        x4 = self.res2(x4, training=training)
+        x4 = self.res3(x4, training=training)
+        x4 = self.res4(x4, training=training)
+        x4 = self.res5(x4, training=training)
+        x4 = self.res6(x4, training=training)
+        x4 = self.res7(x4, training=training)
+        x4 = self.res8(x4, training=training)
+        x4 = self.res9(x4, training=training)
         
-        x13 = self.up1(x12, None, training=training) # (bs, 128, 128, 128)
-        x14 = self.up2(x13, None, training=training) # (bs, 256, 256, 64)
+        x5 = self.up1(x4, None, training=training) # (bs, 128, 128, 128)
+        x6 = self.up2(x5, None, training=training) # (bs, 256, 256, 64)
 
-        x15 = self.last(x14) # (bs, 256, 256, 3)
-        x15 = tf.nn.tanh(x15)
+        x7 = self.last(x6) # (bs, 256, 256, 3)
+        x7 = tf.nn.tanh(x7)
 
-        return x15
+        return x7
 
 
 class Discriminator(tf.keras.Model):
@@ -274,13 +276,13 @@ class Discriminator(tf.keras.Model):
 
 class C_GAN():
     
-    def __init__(self, generator, noise=False):
-        self.name = generator + str(noise) + '_'
-        self.generator = UNetGenerator(noise) if generator == 'unet' else ResNet9Generator(noise)
+    def __init__(self, generator, noise=False, generator_learning_rate=2e-4, discriminator_learning_rate=2e-4):
+        self.name = generator.__class__.__name__ + '_' + str(noise) + '_'
+        self.generator = generator
         self.discriminator = Discriminator()
         
-        self.generator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
-        self.discriminator_optimizer = tf.train.AdamOptimizer(2e-4, beta1=0.5)
+        self.generator_optimizer = tf.train.AdamOptimizer(generator_learning_rate, beta1=0.5)
+        self.discriminator_optimizer = tf.train.AdamOptimizer(discriminator_learning_rate, beta1=0.5)
         
         self.checkpoint_prefix = os.path.join(checkpoint_dir, self.name + "ckpt")
         self.checkpoint = tf.train.Checkpoint(generator_optimizer=self.generator_optimizer,
